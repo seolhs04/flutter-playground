37004fc9d20dea118382e793d34e697b84d7b5a7
seolhs04 <shs00421@gmail.com>
2023-04-03 16:28:25 +0900

    create cartoon app what use launchurl

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:flutter_app/models/webtoon_detail_model.dart';
+import 'package:flutter_app/models/webtoon_episode_model.dart';
 import 'package:flutter_app/models/webtoon_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -19,6 +21,33 @@ class ApiService {
         webtoonInstances.add(webtoonInstance);
       }
       return webtoonInstances;
+    } catch (err) {
+      throw Error();
+    }
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse('$baseUrl/$id');
+    try {
+      final response = await http.get(url);
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    } catch (err) {
+      throw Error();
+    }
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    final url = Uri.parse('$baseUrl/$id/episodes');
+    List<WebtoonEpisodeModel> episodeInstances = [];
+    try {
+      final response = await http.get(url);
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodeInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodeInstances;
     } catch (err) {
       throw Error();
     }
---
+++ lib/widgets/episode_widget.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+import '../models/webtoon_episode_model.dart';
+
+class Episode extends StatelessWidget {
+  final String webtoonId;
+  final WebtoonEpisodeModel episode;
+
+  const Episode({
+    super.key,
+    required this.episode,
+    required this.webtoonId,
+  });
+
+  onButtonTap() async {
+    await launchUrlString(
+        'https://comic.naver.com/webtoon/detail?titleId=$webtoonId&no=${episode.id}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onButtonTap,
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              width: 1,
+              color: Colors.green,
+            ),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 8,
+                offset: const Offset(5, 5),
+                color: Colors.black.withOpacity(0.5),
+              )
+            ],
+            borderRadius: BorderRadius.circular(20)),
+        margin: const EdgeInsets.only(
+          bottom: 10,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 20,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                episode.title,
+                style: const TextStyle(
+                  color: Colors.green,
+                ),
+              ),
+              const Icon(
+                Icons.chevron_right_rounded,
+                color: Colors.green,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
