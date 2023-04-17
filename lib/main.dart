a5d1e9078ceb30bfb39fe9ea555bd6e389855e6b
seolhs04 <shs00421@gmail.com>
2023-04-17 17:44:29 +0900

    update expense screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/screens/expenses/expenses_screen.dart';
+import 'package:flutter_app/screens/expense/expense_screen.dart';
+
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.amber,
-        ),
-        fontFamily: 'Quicksand',
+  runApp(MaterialApp(
+    title: 'Flutter App',
+    theme: ThemeData().copyWith(
+      useMaterial3: true,
+      colorScheme: kColorScheme,
+      appBarTheme: const AppBarTheme().copyWith(
+        backgroundColor: kColorScheme.onPrimaryContainer,
+        foregroundColor: kColorScheme.primaryContainer,
       ),
-      home: const ExpensesScreen(),
-    );
-  }
+      cardTheme: const CardTheme().copyWith(
+        color: kColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 8,
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      textTheme: ThemeData().textTheme.copyWith(
+            titleLarge: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: kColorScheme.onSecondaryContainer,
+              fontSize: 16,
+            ),
+          ),
+    ),
+    home: const ExpenseScreen(),
+  ));
 }
